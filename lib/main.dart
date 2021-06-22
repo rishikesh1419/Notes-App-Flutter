e32e68d0bf58b68b02ea3b364ae49765d54ab75d
Rishikesh Kadam <rishikesh1419@gmail.com>
2021-06-22 23:09:27 +0530

    Root route done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/db/db_provider.dart';
+
+import 'models/note_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,11 +23,73 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  getNotes() async {
+    final notes = await DbProvider.db.getNotes();
+    return notes;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("All Notes"),
+      ),
+      body: FutureBuilder(
+        future: getNotes(),
+        builder: (context, noteData) {
+          switch (noteData.connectionState) {
+            case ConnectionState.waiting:
+              {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            case ConnectionState.done:
+              {
+                if (noteData.data == Null) {
+                  return Center(
+                    child: Text("Create your first note!"),
+                  );
+                } else {
+                  List<NoteModel> data = noteData.data as List<NoteModel>;
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        String title = data[index].title;
+                        String body = data[index].body;
+                        String creationDate =
+                            data[index].creationDate.toString();
+                        int id = data[index].id;
+
+                        return Card(
+                          child: ListTile(
+                            title: Text(title),
+                            subtitle: Text(body),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                }
+              }
+            case ConnectionState.none:
+              return Center(
+                child: Text("Something"),
+              );
+            case ConnectionState.active:
+              return Center(
+                child: Text("Something"),
+              );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.note_add),
+        onPressed: () {
+          // Navigator.pushNamed(context, "/add");
+        },
       ),
     );
   }
